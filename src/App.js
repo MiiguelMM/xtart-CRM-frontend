47b31f7ed0993a4ec66a4deff5058180539b8459
MiiguelMM <miguelmelgarejosanchez@live.com>
2025-05-20 16:34:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.css';
+import './styles/empleados.css';
+import './styles/clientes.css';
+import './styles/ventas.css';
+import './styles/productos.css';
+
+import Index from './components/index.jsx';
+import Clientes from './components/clientes.jsx';
+import Empleados from './components/empleados.jsx';
+import Ventas from './components/ventas.jsx';
+import Productos from './components/productos.jsx';
+import WelcomePage from './components/WelcomePage.jsx';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/index" element={<Index />} />
+          <Route path="/clientes" element={<Clientes />} />
+          <Route path="/empleados" element={<Empleados />} />
+          <Route path="/ventas" element={<Ventas />} />
+          <Route path="/productos" element={<Productos />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
